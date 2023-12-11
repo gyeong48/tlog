bea3bb807199793b1c0da1878d236e1f8b4dd143
gyeong48 <seogyeonghwan48@gmail.com>
2023-12-11 19:23:41 +0900

    initialize settings

---
+++ src/main/java/com/gyeong48/tlog/TlogApplication.java
@@ -0,0 +1,13 @@
+package com.gyeong48.tlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gyeong48/tlog/TlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gyeong48.tlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
